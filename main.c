85fbc1ca7a4b7ffeda44fc8f823635f9bcb7bd32
Samuel Guadagnini <c3304667@uon.edu.au>
2019-04-03 08:52:23 +0000

    actual changes made

--- main.c
+++ main.c
@@ -2,8 +2,20 @@
 
 int main() {
 
-just putting in crap
-more crap
-just testing commit
+int key, x;
+char message [1000];
+
+printf ("Please input a number to pick from the following options to be performed\n");
+printf ("1 = Encryption with a rotation cipher given plain text and key\n");
+printf ("2 = Decryption with a rotation cipher given cipher text and key\n");
+printf ("3 = Encryption with a substitution cipher given plain text and key\n");
+printf ("4 = Decryption with a substitution cipher given cipher text and key\n");
+
+scanf("%d", &x);
+
+switch (x) {
+    case 1: for(key = 0; )
+}
+
 return 0;
 }
