ec042291cafc263d508a2554efb35e7770b81872
Samuel Guadagnini <c3304667@uon.edu.au>
2019-04-10 09:43:32 +0000

    slightly changed. still needs work. fixed up but load of errors. index still needs to be assigned to the message index

--- main.c
+++ main.c
@@ -4,27 +4,32 @@ int main() {
 
 char key; 
 int x;
-char message [];
+char message[1000] = {";
 int i;
 
-printf ("Please input a number to pick from the following options to be performed\n");
-printf ("1 = Encryption with a rotation cipher given plain text and key\n");
-printf ("2 = Decryption with a rotation cipher given cipher text and key\n");
-printf ("3 = Encryption with a substitution cipher given plain text and key\n");
-printf ("4 = Decryption with a substitution cipher given cipher text and key\n");
+printf("Please input a number to pick from the following options to be performed\n");
+printf("1 = Encryption with a rotation cipher given plain text and key\n");    //this is all the menu to begin with to select what is being tested
+printf("2 = Decryption with a rotation cipher given cipher text and key\n");
+printf("3 = Encryption with a substitution cipher given plain text and key\n");
+printf("4 = Decryption with a substitution cipher given cipher text and key\n");
 
 scanf("%d", &x);
 
-switch (x=1) {
-    case 1: printf ("enter key used to encrypt");
-    scanf ("%c", &key);
-    printf ("enter message to be encrypted");
-    scanf ("%c", &message);
-    if(message[]>=65 && message<=90) {
-        message[]-65=message[];
-        ((message[]+key)%26) = message[];
-        message[] = message[] +65;
+switch (x) {
+    case 1: printf("enter key used to encrypt and press enter\n");
+    scanf("%c", &key);
+    printf("then enter message to be encrypted\n");                    
+    scanf("%s", message);
+    for (int i=0; i<1001; i++){
+    if(message[i]>=65 && message[i]<=90) {
+            message[i] = message[i]-65;
+            message[i] = ((message[i]+key)%26);
+            message[i] = message[i] +65;                            //entered into codeboard, so far it prints
+         
+        }
     }
+       printf("the encrypted message is %s", message);
+
 }
 
 return x;
